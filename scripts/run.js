1b60111612ae0515ac7fff0eb0f6d14bd428b73f
utja <28519719+utja@users.noreply.github.com>
2022-03-14 21:56:27 -0400

    storing messages in arrays using structs

--- scripts/run.js
+++ scripts/run.js
@@ -1,24 +1,23 @@
 const main = async () => {
-	const [owner, randomPerson] = await hre.ethers.getSigners();
 	const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
 	const waveContract = await waveContractFactory.deploy();
 	await waveContract.deployed();
-
+	
 	console.log("Contract deployed to:", waveContract.address);
-	console.log("Contract deployed by:", owner.address);
 	
 	let waveCount;
 	waveCount = await waveContract.getTotalWaves();
-
-	let waveTxn = await waveContract.wave();
+	console.log(waveCount.toNumber());
+	
+	let waveTxn = await waveContract.wave('A message!');
+	await waveTxn.wait();
+	
+	const [_, randomPerson] = await hre.ethers.getSigners();
+	waveTxn = await waveContract.connect(randomPerson).wave('Another message!');
 	await waveTxn.wait();
 
-	waveCount = await waveContract.getTotalWaves();
-
-	waveTxn = await waveContract.connect(randomPerson).wave();
-	await waveTxn.wait();
-
-	waveCount = await waveContract.getTotalWaves();
+	let allWaves = await waveContract.getAllWaves();
+	console.log(allWaves);
 };
 
 const runMain = async () => {
